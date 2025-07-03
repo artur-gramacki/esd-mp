259740ff94906740a823258262efa6aef6001938
Artur Gramacki <89815117+artur-gramacki@users.noreply.github.com>
2025-07-03 22:49:02 +0200

    Add files via upload

---
+++ edf_to_hdf5/R/__RUN__.R
@@ -0,0 +1,113 @@
+# file __RUN__.R
+# copyright (C) 2022-2025 Artur Gramacki and Jarosław Gramacki
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+# ///////////////////////////////////////////////////////////////////////////////////////////
+# Uncomment if you are sure you want to remove the given directories
+# ///////////////////////////////////////////////////////////////////////////////////////////
+
+dir <- "../"
+
+unlink(paste(dir, 'working/', sep = ""), recursive = TRUE)
+dir.create(paste(dir, 'working/', sep = ""))
+dir.create(paste(dir, 'working/aux_files', sep = ""))
+dir.create(paste(dir, 'working/bin_files', sep = ""))
+dir.create(paste(dir, 'working/bin_files/eA_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/bin_files/eB_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/bin_files/eC_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/hdf5_files', sep = ""))
+dir.create(paste(dir, 'working/sqlitedb_files', sep = ""))
+dir.create(paste(dir, 'working/sqlitedb_files/eA_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/sqlitedb_files/eB_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/sqlitedb_files/eC_w10_c20', sep = ""))
+dir.create(paste(dir, 'working/tf_maps', sep = ""))
+dir.create(paste(dir, 'working/tf_maps/eA_w10_c20_64_64', sep = ""))
+dir.create(paste(dir, 'working/tf_maps/eB_w10_c20_64_64', sep = ""))
+dir.create(paste(dir, 'working/tf_maps/eC_w10_c20_64_64', sep = ""))
+
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# --- IMPORTANT NOTE ---
+# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# Before starting, download the dataset of neonatal EEG recordings to the 'edf' directory.  
+# These are available at https://zenodo.org/record/4940267. 
+# There are 79 EDF files and 3 CSV annotations files. 
+# The EDF files are approximately 4GB in size.  
+
+source("edf_to_bin.R")
+source("bin_to_db.R")
+source("db_to_RData.R")
+
+# ///////////////////////////////////////////////////////////////////////////////////////////
+# Some examples
+
+source("my_custom_palette.R")
+
+# Display time-frequency map on the screen
+out <- mp2tf(
+  SQLiteFile = "example.db", 
+  channel = 1, 
+  mode = "sqrt", 
+  freqDivide = 4,
+  increaseFactor = 16,
+  displayCrosses = TRUE, 
+  grid = FALSE, 
+  crossesColor = "white", 
+  palette = "my custom palette", 
+  rev = TRUE,
+  outMode = "plot"
+)
+
+# Save time-frequency map to file
+out <- mp2tf(
+  SQLiteFile = "example.db", 
+  channel = 1, 
+  mode = "sqrt", 
+  freqDivide = 4,
+  increaseFactor = 16,
+  displayCrosses = TRUE, 
+  crossesColor = "white", 
+  grid = FALSE, 
+  palette = "my custom palette", 
+  outMode = "file",
+  fileName = "example.png",
+  fileSize = c(512, 512),
+)
+
+# Save time-frequency map to RData file
+out <- mp2tf(
+  SQLiteFile = "example.db", 
+  channel = 1, 
+  mode = "sqrt", 
+  freqDivide = 4,
+  increaseFactor = 16,
+  displayCrosses = FALSE, 
+  grid = FALSE, 
+  palette = "my custom palette", 
+  outMode = "RData",
+  fileName = "example.RData",
+  fileSize = c(64, 64),
+)
+
+# Read Rdata file form disk and display it's content on the screen
+# t-f maps are 64x64 pixels size
+load("example.RData")
+par(pty = "s")
+graphics::image(
+  x = seq(0, 10, length.out = 64), 
+  y = seq(0, 32, length.out = 64), 
+  z = tf.matrix, 
+  col = my_custom_palette,
+  xlab = "t [s]",
+  ylab = "f [Hz]" )
---
+++ edf_to_hdf5/R/edf_to_bin.R
@@ -0,0 +1,79 @@
+# file edf_to_bin.R
+# copyright (C) 2022-2025 Artur Gramacki and Jarosław Gramacki
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 or 3 of the License
+#  (at your option).
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  A copy of the GNU General Public License is available at
+#  http://www.r-project.org/Licenses/
+
+source("funs.R")
+
+dir = "../"
+
+# Check if edf files are already downloaded into the 'edf' dir.
+files <- list.files(paste(dir . "edf", , sep = ""), include.dirs = FALSE, pattern <- patterns[i])
+
+# Symbols of human experts
+we <- c( "A", "B", "C")
+
+# Annotations file names, as downloaded from https://zenodo.org/record/4940267
+ann.f <- c("annotations_2017_A_fixed.csv",
+          "annotations_2017_B.csv",
+          "annotations_2017_C.csv")
+
+# infant IDs which have seizures 
+s.IDs <- c(1,4,5,7,9,11,13,14,15,16,17,19,20,21,22,25,31,34,36,38,39,40,41,44,47,50,51,52,62,63,66,67,69,71,73,75,76,77,78,79)
+# for testitng it's better to use only a subset
+# s.IDs <- c(4,5)
+
+# infant IDs which are seizure free
+ns.IDs <- c(3,10,18,27,28,29,30,32,35,37,42,45,48,49,53,55,57,58,59,60,70,72)
+# for testitng it's better to use only a subset
+# ns.IDs <- c(3,10)
+
+my.seeds = c(42, 1024, 9999)
+
+timestamp()
+for (i in 1:3) {
+  out <-  generate_samples(
+    which.expert = we[i], 
+    annotations_file = ann.f[i], 
+    seizure.IDs = s.IDs, 
+    non.seizure.IDs = ns.IDs, 
+    window = 10, 
+    chunks = 20, 
+    down.sampling.factor = 1, 
+    filtering = TRUE, 
+    dir = dir, 
+    random = FALSE, 
+    my.seed = my.seeds[i],
+    write.txt.files = FALSE, 
+    write.hdf5.files = FALSE,
+    write.bin.files = TRUE,
+    write.bin.as.txt.files = FALSE
+    )
+  }
+timestamp()
+
+files <- list.files("../working/bin_files", include.dirs = TRUE, pattern = ".*eA.*.bin")
+from_pathes <- file.path("../working/bin_files", files)
+to_pathes <- file.path("../working/bin_files/eA_w10_c20",files)
+out <- file.rename(from = from_pathes, to = to_pathes)
+
+files <- list.files("../working/bin_files", include.dirs = TRUE, pattern = ".*eB.*.bin")
+from_pathes <- file.path("../working/bin_files", files)
+to_pathes <- file.path("../working/bin_files/eB_w10_c20",files)
+out <- file.rename(from = from_pathes, to = to_pathes)
+
+files <- list.files("../working/bin_files", include.dirs = TRUE, pattern = ".*eC.*.bin")
+from_pathes <- file.path("../working/bin_files", files)
+to_pathes <- file.path("../working/bin_files/eC_w10_c20",files)
+out <- file.rename(from = from_pathes, to = to_pathes)
