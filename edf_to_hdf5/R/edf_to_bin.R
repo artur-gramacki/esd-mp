a9c89d4b5f11f2f4a3ee9e59ade7a1369dc0d5eb
Artur Gramacki <89815117+artur-gramacki@users.noreply.github.com>
2025-07-04 10:48:12 +0200

    Update edf_to_bin.R

--- edf_to_hdf5/R/edf_to_bin.R
+++ edf_to_hdf5/R/edf_to_bin.R
@@ -21,11 +21,7 @@ dir = "../"
 # Check if edf files are already downloaded into the 'edf' dir.
 files <- list.files(paste(dir, "edf", sep = ""), include.dirs = FALSE, pattern <- ".edf")
 if (length(files) != 79) {
-  stop(
-  "Download to `edf` directory 79 EDF files of neonatal EEG recordings from https://zenodo.org/record/4940267. 
-  See also `__read_me__.txt` file.
-  "
-)
+  stop("Download to `edf` directory 79 EDF files of neonatal EEG recordings from https://zenodo.org/record/4940267.")
 }
 
 
